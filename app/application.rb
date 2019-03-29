28dd9c47cae2374deb5070db6f2cd3e607073db9
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-29 13:17:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,8 @@
 class Application
   def call
     resp = Rack::Response.new
-    runtime = time.now
+    runtime = time.hour
+    resp.write "#{runtime}"
 
     if runtime < 12:00pm
       resp.write "Good Morning!"
