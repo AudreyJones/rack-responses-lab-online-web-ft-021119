abb6097d5887a79beb23559c6efd961f4685be53
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-29 13:16:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,14 @@
 class Application
   def call
+    runtime = time.
 
+    if runtime < 12:00pm
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+
+    resp.finish
   end
 
 end
