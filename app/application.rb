9fc7b763e5a6bfac049a9d5367d1038be04f9556
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-29 13:24:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     # binding.pry
     runtime = Time.now
 
-    resp.write "The time is now: #{runtime}/n"
+    resp.write "The time is now: #{runtime}\n"
 
     if runtime.hour <= 12
       resp.write "Good Morning!"
